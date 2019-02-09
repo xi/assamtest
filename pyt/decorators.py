116ba89dc51c76830b78f598282a4116bcbc47bf
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-02-09 08:47:33 +0100

    add async_test decorator

--- pyt/decorators.py
+++ pyt/decorators.py
@@ -1,14 +1,26 @@
+import asyncio
+
 from .expect import expect
 
 
 def fail(fn):
-	def wrapper():
+	def wrapper(*args, **kwargs):
 		with expect.raises(AssertionError):
-			fn()
+			fn(*args, **kwargs)
 	return wrapper
 
 
 def skip(fn):
-	def wrapper():
+	def wrapper(*args, **kwargs):
 		pass
+	return wrapper
+
+
+def async_test(fn):
+	def wrapper(*args, **kwargs):
+		coro = asyncio.coroutine(fn)
+		future = coro(*args, **kwargs)
+		loop = asyncio.get_event_loop()
+		loop.run_until_complete(future)
+		loop.close()
 	return wrapper
