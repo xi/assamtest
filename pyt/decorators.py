f824e4e7e8909cf9fde430af933336930b5f2848
Tobias Bengfort <tobias.bengfort@posteo.de>
2019-02-09 08:51:01 +0100

    use functools.wraps()

--- pyt/decorators.py
+++ pyt/decorators.py
@@ -1,9 +1,11 @@
 import asyncio
+import functools
 
 from .expect import expect
 
 
 def fail(fn):
+	@functools.wraps(fn)
 	def wrapper(*args, **kwargs):
 		with expect.raises(AssertionError):
 			fn(*args, **kwargs)
@@ -11,12 +13,14 @@ def fail(fn):
 
 
 def skip(fn):
+	@functools.wraps(fn)
 	def wrapper(*args, **kwargs):
 		pass
 	return wrapper
 
 
 def async_test(fn):
+	@functools.wraps(fn)
 	def wrapper(*args, **kwargs):
 		coro = asyncio.coroutine(fn)
 		future = coro(*args, **kwargs)
